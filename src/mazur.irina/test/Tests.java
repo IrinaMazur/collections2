2093049d06a27876ca607b17be58d639419b4fe5
IrinaMazur <irinka.mazur.04@gmail.com>
2024-12-01 00:21:56 +0300

    update program files

---
+++ src/mazur.irina/test/Tests.java
@@ -0,0 +1,35 @@
+package mazur.irina.test;
+
+import mazur.irina.project.Person;
+import mazur.irina.project.Subdivision;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AssertTrue;
+
+/**
+ * Unit tests of the Person class
+ */
+public class Tests {
+
+    Person person = new Person("1", "Name", "Sex", "15.12.2021", "1000", new Subdivision(1, "A"));
+
+    @Test
+    public void Test_Id() {
+        AssertEquals();
+    }
+
+    @Test
+    public void Test_Name() {
+
+    }
+
+    @Test
+    public void Test_Id() {
+
+    }
+
+    @Test
+    public void Test_Id() {
+
+    }
+
+}
